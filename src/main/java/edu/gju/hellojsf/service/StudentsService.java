b8acd1b4fbf0262a3d99490e88f870ca6e5bd1a4
hbata <hesham.bataineh@aiesec.net>
2017-10-15 00:34:10 +0300

    Added Student service to retrive students from DB

---
+++ src/main/java/edu/gju/hellojsf/service/StudentsService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.gju.hellojsf.service;
+
+import edu.gju.hellojsf.entities.Student;
+import edu.gju.hellojsf.repository.StudentRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hesham
+ */
+@Service("studentBean")
+public class StudentsService {
+
+    @Autowired
+    private StudentRepository studentReposiory;
+
+    public List<Student> findAll() {
+        return studentReposiory.findAll();
+    }
+
+    public StudentRepository getStudentReposiory() {
+        return studentReposiory;
+    }
+
+    public void setStudentReposiory(StudentRepository studentReposiory) {
+        this.studentReposiory = studentReposiory;
+    }
+
+}
